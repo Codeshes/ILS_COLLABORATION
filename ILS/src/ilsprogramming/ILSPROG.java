936e7417e6cb63b0aa41e8cf2e82f9e736fee8c9
Nat Nats <vargasnatnat10@gmail.com>
2025-09-26 18:59:03 +0800

    NAT COMMit 2

--- ILS/src/ilsprogramming/ILSPROG.java
+++ ILS/src/ilsprogramming/ILSPROG.java
@@ -4,6 +4,10 @@ public class ILSPROG {
 
 	public static void main(String[] args) {
 		System.out.println("TANGINA NIYONG LAHAT");
-		System.out.println("I love sti");	}
+		System.out.println("I love sti");	
+		System.out.println("I LOVE STI SO MUCH");
+	
+	
+	}
 
 }
