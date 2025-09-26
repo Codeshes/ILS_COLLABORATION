b1a10bec7e4bee4a9c008062bfda344e55dccfb4
aquin <aquin@LAPTOP-G77A8BDI>
2025-09-26 19:03:40 +0800

    IVAN COMMIT

--- ILS/src/ilsprogramming/ILSPROG.java
+++ ILS/src/ilsprogramming/ILSPROG.java
@@ -4,10 +4,13 @@ public class ILSPROG {
 
 	public static void main(String[] args) {
 		System.out.println("TANGINA NIYONG LAHAT");
+<<<<<<< HEAD
 		System.out.println("I love sti");	
 		System.out.println("I LOVE STI SO MUCH");
 		System.out.println("I am nat");
 	
+=======
+>>>>>>> 237c3e7 (IVAN COMMIT)
 	}
 
 }
